27021ccb07f1f0e506c6c4ca1d93ef1256bf50db
ayana hirakawa <ayanahirakawa@ayananoMacBook-Pro.local>
2026-01-07 19:07:14 +0900

    マイグレーション完了

--- src/database/migrations/2026_01_07_131751_create_items_table.php
+++ src/database/migrations/2026_01_07_131751_create_items_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateItemsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('items', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('brand')->nullable();
+            $table->integer('price')->unsigned();
+            $table->text('description');
+            $table->string('condition');
+            $table->string('image_url');
             $table->timestamps();
         });
     }
@@ -29,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('items');
     }
 }
